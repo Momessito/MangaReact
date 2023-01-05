1f613b3c588fdd198a44a283186899170a064076
unknown <Pinhoes1000@gmail.com>
2023-01-05 19:27:02 -0300

    Daily

--- src/MangaPage.js
+++ src/MangaPage.js
@@ -66,9 +66,12 @@ function Manga() {
 
             <SideMenu />
             <Nav />
+            <div className='BackTope' onClick={totop}>
+        <svg className='Totop bi bi-arrow-down-right' xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+  <path fillRule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0v6z"/>
+</svg>
+</div>
             <div className='mainManga'>
-
-
                 {posts.length === 0 ? <p>Carregando</p> : (
                     posts.map((post) => (
                         <img src={post.avif} key={post.avif} alt='NaN' className='PageManga' />
