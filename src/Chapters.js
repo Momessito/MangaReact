d64d97d878756a2b44cf51d85c67df58b63871e8
Felipe <Pinhoes1000@gmail.com>
2023-01-06 23:41:06 -0300

    daily

--- src/Chapters.js
+++ src/Chapters.js
@@ -54,7 +54,6 @@ const Chapters = () => {
             const response = await axios.get(url);
 
             const data = response.data;
-            console.log(response)
 
             setimg(data)
         } catch (Error) {
@@ -73,24 +72,25 @@ const Chapters = () => {
 
             <Nav />
             <div className='Black'></div>
-
+<div className='allchapters'>
             <div className='chapter-visual'>
                 <img width='300px' src={img.image} alt={img.id} />
                 <div>
                     <h1>{img.title}</h1>
                     <h2>Autor: {img.author}</h2>
+                    <h3 className='score'>Nota: {img.score}⭐</h3>
                     <h4>Descrição: {img.description}</h4>
                 </div>
             </div>
             <div className="Filter">
                 <div className=''>
                     <svg onClick={descer} xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="filterDown bi bi-arrow-down-up" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z" />
+                        <path fillRule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z" />
                     </svg>
                 </div>
-                <div className='filterI'><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <div className='filterI'><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-</svg><input placeholder='Pesquisar...'/>
+</svg><input placeholder='Pesquisar...' id='pesquisa' onKeyUpCapture={pesquisar}/>
                 </div>
 
             </div>
@@ -101,15 +101,21 @@ const Chapters = () => {
                         <Link to={'/chapter/' + post.release_id} key={post.release_id}>
                             <div className='ChaptersCard' id='ChaptersCard'>
                                 <h2>{post.name}</h2>
-                                <h4>{post.number} {post.chapter_name}</h4>
+                                <h4 id='capit'>{post.number} {post.chapter_name}</h4>
                                 <small>{post.date}</small>
                             </div>
                             <div className='wr2'></div>
                         </Link>
                     )))}
             </div>
+            </div>
         </div>
     )
+
+    function pesquisar(){
+
+
+    }
 
     function descer() {
         if (istrue === true) {
